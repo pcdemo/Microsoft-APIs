ea11bd0ba5abd3f3f3428db514e1e9b302f5c2fd
pcdemo <37341806+pcdemo@users.noreply.github.com>
2018-07-06 15:01:29 +0200

    Add files via upload

---
+++ Preconsent.ps1
@@ -0,0 +1,17 @@
+﻿# Check version
+# Get-Module -Name AzureAD
+# Install-Module AzureAD -Force
+Connect-AzureAD 
+
+$AppId = 'INSERT-APPLICATION-ID-HERE'
+$DisplayName = 'INSERT-APPLICATION-DISPLAY-NAME-HERE'
+
+$g = Get-AzureADGroup | ? {$_.DisplayName -eq 'AdminAgents'}
+Get-AzureADGroupMember -ObjectId $g.ObjectId
+$s = Get-AzureADServicePrincipal -All $true  | ? {$_.AppId -eq $AppId}
+
+if ($s -eq $null) { $s = New-AzureADServicePrincipal -AppId $AppId -DisplayName $DisplayName }
+Add-AzureADGroupMember -ObjectId $g.ObjectId -RefObjectId $s.ObjectId
+
+# The service principal for application can be removed if required with
+#Remove-AzureADGroupMember -ObjectId $g.ObjectId -MemberId $s.ObjectId
